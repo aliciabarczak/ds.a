932ae6c72b8cf1aa9efc12d204b60cc944603c11
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-07-25 15:16:06 +0100

    add write file to the build.js script and lib folder to the src folder to allow a new file to be created and written with the converted css

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -2,9 +2,11 @@ const Fs = require("fs");
 const Path = require("path");
 const Sass = require("node-sass");
 
-Sass.renderSync({
+const result = Sass.renderSync({
   data: Fs.readFileSync(Path.resolve("src/global.scss")).toString(),
   outputStyle: "expanded",
   outFile: "global.css",
-  includePaths: [Path.resolve("src")]
+  includePaths: [Path.resolve("src")],
 });
+
+Fs.writeFileSync(Path.resolve("src/lib/global.css"), result.css.toString());

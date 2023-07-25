02bff006e15df42d23072a415cbef9f57ff1adce
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-07-25 15:26:08 +0100

    re-factor the build.js script so that it is re-usable for all atoms/molecules and organisms that need to be complied and use the same for comppiling global.scss into scc

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -2,11 +2,15 @@ const Fs = require("fs");
 const Path = require("path");
 const Sass = require("node-sass");
 
-const result = Sass.renderSync({
-  data: Fs.readFileSync(Path.resolve("src/global.scss")).toString(),
-  outputStyle: "expanded",
-  outFile: "global.css",
-  includePaths: [Path.resolve("src")],
-});
+const compile = (origin, destination) => {
+    result = Sass.renderSync({
+        data: Fs.readFileSync(Path.resolve(origin)).toString(),
+        outputStyle: "expanded",
+        outFile: "global.css",
+        includePaths: [Path.resolve("src")],
+      });
 
-Fs.writeFileSync(Path.resolve("src/lib/global.css"), result.css.toString());
+      Fs.writeFileSync(Path.resolve(destination), result.css.toString());
+}
+  
+compile("src/global.scss", "src/lib/global.css");

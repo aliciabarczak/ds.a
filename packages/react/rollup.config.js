48b968fe5f19b1be97072357d4ed9b24d832792e
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-07-27 15:28:24 +0100

    fix config error

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,12 +1,12 @@
 import Ts from "rollup-plugin-typescript2";
 
 export default {
-  input: ["src/index.js"],
+  input: ["src/index.ts"],
   output: {
     dir: "lib",
     format: "esm",
     sourcemap: true,
+    preserveModules: true, 
   },
   plugins: [Ts()],
-  preserveModes: true,
 };

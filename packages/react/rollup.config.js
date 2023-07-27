55a67c284f551328195bf7eb631a82a76af0b7fa
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-07-27 15:39:17 +0100

    add another input to enable the Button component to be imported independently of other components

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,12 +1,12 @@
 import Ts from "rollup-plugin-typescript2";
 
 export default {
-  input: ["src/index.ts"],
+  input: ["src/index.ts", "src/atoms/Button/index.ts"],
   output: {
     dir: "lib",
     format: "esm",
     sourcemap: true,
-    preserveModules: true, 
+    preserveModules: true,
   },
   plugins: [Ts()],
 };
